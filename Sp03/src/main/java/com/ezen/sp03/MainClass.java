0d501428f2cf8a899e000e54ed9ed75b039a93b7
Java02 <Java02@DESKTOP-RDLU2MK>
2021-12-06 22:14:03 +0900

    Create Sp03 Maven Product

---
+++ Sp03/src/main/java/com/ezen/sp03/MainClass.java
@@ -0,0 +1,10 @@
+package com.ezen.sp03;
+
+public class MainClass {
+	public static void main(String[] args) {
+		// WalkClass wc = new WalkClass();
+		// wc.move();
+		
+		
+	}
+}
---
+++ Sp03/src/main/java/com/ezen/sp03/WalkClass.java
@@ -0,0 +1,7 @@
+package com.ezen.sp03;
+
+public class WalkClass {
+	public void move() {
+		System.out.println("걸어서 이동합니다.");
+	}
+}
