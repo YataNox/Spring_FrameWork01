e69aa803b54707547d10628dd8fa427fef867db0
Java02 <Java02@DESKTOP-RDLU2MK>
2021-12-06 22:19:30 +0900

    WalkClass beans Add & MainClass Write

--- Sp03/src/main/java/com/ezen/sp03/MainClass.java
+++ Sp03/src/main/java/com/ezen/sp03/MainClass.java
@@ -1,10 +1,15 @@
 package com.ezen.sp03;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 	public static void main(String[] args) {
 		// WalkClass wc = new WalkClass();
 		// wc.move();
 		
-		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		WalkClass wc = ctx.getBean("cWalk", WalkClass.class);
+		wc.move();
+		ctx.close();
 	}
 }
