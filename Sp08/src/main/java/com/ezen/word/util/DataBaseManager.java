0d2c3bd3f307b0b9d64388481c1ce54753a4f594
JAVA02 <JAVA02@DESKTOP-PHS3PBS>
2021-12-08 20:54:03 +0900

    appCtx.xml Constructor Dependency minimization

--- Sp08/src/main/java/com/ezen/word/util/DataBaseManager.java
+++ Sp08/src/main/java/com/ezen/word/util/DataBaseManager.java
@@ -6,24 +6,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DataBaseManager {
-	private String driver;
-	private String url;
-	private String id;
-	private String pw;
+	// private String driver;
+	// private String url;
+	// private String id;
+	// private String pw;
 	
-	public DataBaseManager(DataBaseUserinfo dbi) {
+	@Autowired // 스프링 컨테이너에서 해당 객체를 스스로 검색 후 꺼내서 아래 레퍼런스 변수에 할당합니다.
+	private DataBaseUserinfo dbi;
+	// appCtx.xml 안에서 Constructor-args를 사용하지 않고, 직접 DataBaseUserinfo 객체를
+	// 꺼내다가 dbi 변수에 넣어서 사용할 수 있게 합니다.
+	
+	/*public DataBaseManager(DataBaseUserinfo dbi) {
 		url = dbi.getUrl();
 		id = dbi.getId();
 		pw = dbi.getPw();
 		driver = dbi.getDriver();
-	}
+	}*/
 	
 	public Connection getConnection() {
 		Connection con = null;
 		try {
-			Class.forName(driver);
-			con = DriverManager.getConnection(url, id, pw);
+			Class.forName(dbi.getDriver());
+			con = DriverManager.getConnection(dbi.getUrl(), dbi.getId(), dbi.getPw());
 		}catch(ClassNotFoundException e) {
 			e.printStackTrace();
 		}catch (SQLException e) {
